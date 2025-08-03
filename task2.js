c5516be4ec4d307f8b2846a4e973da1fcced8749
Yeamin-HS <yeamin.hossain.shihab@g.bracu.ac.bd>
2025-08-03 21:18:32 +0600

    Task 2 is done

---
+++ task2.js
@@ -0,0 +1,15 @@
+var money=10000;
+if (money>0 && money<=10**9)  {
+if (money >=25000) {
+    console.log("Laptop"); }
+else if (money >= 10000 && money < 25000) {
+    console.log("Cycle"); }  
+
+else{ console.log("Chocolate"); }  }    
+
+
+else{console.log("Invalid value");}
+
+
+
+
