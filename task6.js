9bc4988e4d8720dc5bacebd58d385a6323ec7e3b
Yeamin-HS <yeamin.hossain.shihab@g.bracu.ac.bd>
2025-08-04 21:39:57 +0600

    Task 6  is done

---
+++ task6.js
@@ -0,0 +1,12 @@
+var experience = 40;
+var startingSalary = 30000;
+
+//here i have use compound interest formula which is p(1+r/100)^n
+
+var finalSalary = startingSalary * Math.pow((1 + 0.05), experience);
+
+
+console.log(finalSalary.toFixed(2));
+
+
+
